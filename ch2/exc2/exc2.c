f1ac100cc399da98e3a50b16f2065d47945f06f2
Patilmaitry23 <maitrypatil04@gmail.com>
2025-10-30 09:24:28 +0000

    change for loop to while loop

--- ch2/exc2/exc2.c
+++ ch2/exc2/exc2.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #define MAXLINE 1000 /* maximum input line length */
-int getline(char line[], int maxline);
+
+int get_line(char line[], int maxline);
+
 void copy(char to[], char from[]);
+
 /* print the longest input line */
 
-main()
+void main()
 {
     int len; /* current line length */
     int max; /* maximum length seen so far */
     char line[MAXLINE]; /* current input line */
     char longest[MAXLINE]; /* longest line saved here */
     max = 0;
-    while ((len = getline(line, MAXLINE)) > 0)
+    while ((len = get_line(line, MAXLINE)) > 0)
         if (len > max)
         {
             max = len;
@@ -22,7 +25,7 @@ main()
     return 0;
 }
 /* getline: read a line into s, return length */
-int getline(char s[],int lim)
+int get_line(char s[],int lim)
 {
     int c, i;
 
